fdbcadaaf099428241331724b003caeb7dfb5fd4
MAHAJAISHREE R <91447347+Mahajaishree@users.noreply.github.com>
2022-08-30 07:21:29 +0530

    Create Form11.py

---
+++ Form11.py
@@ -0,0 +1,23 @@
+from ._anvil_designer import Form11Template
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form11(Form11Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+    
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Form2')
+    pass
+ 
